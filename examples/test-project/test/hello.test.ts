e62c93b09abae5a39011683177769910ea4c5363
Christ Andreas <AChrist+pcs@pcs.com>
2025-12-25 20:17:49 +0100

    folder renamed

--- examples/test-project/test/hello.test.ts
+++
@@ -1,9 +0,0 @@
-// tests/hello.test.ts
-import { describe, it, expect } from 'vitest';
-
-describe("HelloWorld", () => {
-  it("sollte 'Hello World' zurückgeben", () => {
-    // TODO: Implementieren
-    expect(hello()).toBe("Hello World");
-  });
-});
